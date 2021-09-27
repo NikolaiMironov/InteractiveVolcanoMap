d5595511e71a6d8130941a8835b66c26544dce4e
Nikolay Mironov <nikolai.mironov.1@gmail.com>
2021-09-27 19:45:55 +0300

    New colors and shapes
    adding colors and changing the shape of the marker

--- Mapping.py
+++ Mapping.py
@@ -7,13 +7,29 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
 
+
+#formatting and color chnage of popups
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+def ColorChange(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 #map creation
 map = folium.Map(location= [40.431874, -98.525470], zoom_start=5, tiles = "Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup = str(el) + "m", icon=folium.Icon(color='green')))
-map.add_child(fg)
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln],radius= 7, popup=folium.Popup(iframe), color = ColorChange(el)))
 
-map.save("Map_with_simplePops.html")
+map.add_child(fg)
+map.save("Map_adv_popups.html")
